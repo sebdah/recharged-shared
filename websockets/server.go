c0428a0dc911d2e589fc4ae61cad5a45b388e969
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2015-05-28 21:04:18 +0200

    Provide chargepoint id in pings

--- websockets/server.go
+++ websockets/server.go
@@ -3,16 +3,19 @@ package websockets
 import (
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 )
 
 type Server struct {
-	Conn            *websocket.Conn
-	Upgrader        websocket.Upgrader
-	ReadBufferSize  int
-	WriteBufferSize int
-	WriteChannel    chan string
-	ReadChannel     chan string
+	Conn                    *websocket.Conn
+	Upgrader                websocket.Upgrader
+	ChargePoint             string
+	ReadBufferSize          int
+	WriteBufferSize         int
+	WriteChannel            chan string
+	ReadChannel             chan string
+	PingNotificationChannel chan string
 }
 
 func NewServer() (server *Server) {
@@ -36,6 +39,9 @@ func NewServer() (server *Server) {
 // Handler registering connections
 func (this *Server) Handler(w http.ResponseWriter, r *http.Request) {
 	var err error
+	vars := mux.Vars(r)
+	this.ChargePoint = vars["chargePoint"]
+
 	this.Conn, err = this.Upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Error("Error upgrading websocket connection: %s\n", err)
@@ -44,14 +50,23 @@ func (this *Server) Handler(w http.ResponseWriter, r *http.Request) {
 
 	// Register ping handler
 	this.Conn.SetPingHandler(func(message string) error {
-		log.Debug("Received ping message")
+		log.Debug("Received ping message from '%s'", message)
 		this.Conn.WriteMessage(websocket.PongMessage, []byte(message))
+
+		if this.PingNotificationChannel != nil {
+			this.PingNotificationChannel <- message
+		}
 		return nil
 	})
 
 	// Instanciate a new communicator
 	communicator := NewCommunicator(this.Conn)
-	log.Debug("Starting websockets communication channel")
+	log.Debug("Starting websockets communication channel for Charge Point '%s'", this.ChargePoint)
 	go communicator.Reader(this.ReadChannel)
 	go communicator.Writer(this.WriteChannel)
+}
+
+// Set ping notification channel
+func (this *Server) SetPingNotificationChannel(c chan string) {
+	this.PingNotificationChannel = c
 }
